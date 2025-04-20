dc89ae32fa99efbe33a585c45da63211908d261f
yuriipidlisnyi <yuriipidlisnyi2021@gmail.com>
2025-04-21 00:41:40 +0300

    feat: add security plugin to eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,12 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
-
+import pluginSecurity from "eslint-plugin-security";
 
 export default defineConfig([
   globalIgnores(["node_modules", "dist", "build", "coverage"]),
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"]},
+  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js, pluginSecurity }, extends: ["js/recommended"]},
   { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser }},
   tseslint.configs.recommended,
+  pluginSecurity.configs.recommended
 ]);
